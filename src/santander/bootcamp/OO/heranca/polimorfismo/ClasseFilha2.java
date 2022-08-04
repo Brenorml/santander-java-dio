2120728ab853608d342561383d1f63cf4aafb316
Brenorml <breno.lucena@gmail.com>
2022-08-04 21:43:45 +0100

    Adicionado o exercicio 3 sobre herança - polimorfismo - OO

---
+++ src/santander/bootcamp/OO/heranca/polimorfismo/ClasseFilha2.java
@@ -0,0 +1,13 @@
+package santander.bootcamp.OO.heranca.polimorfismo;
+
+public class ClasseFilha2 extends ClasseMae {
+    @Override
+    void metodo1() {
+        System.out.println("Método 1 da classe Filha 2");
+    }
+
+    @Override
+    void metodo2() {
+        System.out.println("Método 2 da classe filha 2");
+    }
+}
---
+++ src/santander/bootcamp/OO/heranca/polimorfismo/ClasseMae.java
@@ -0,0 +1,12 @@
+package santander.bootcamp.OO.heranca.polimorfismo;
+
+public class ClasseMae {
+
+    void metodo1() {
+        System.out.println("Método 1 da classe Mãe");
+    }
+
+    void metodo2() {
+        System.out.println("Método 2 da classe Mãe");
+    }
+}
